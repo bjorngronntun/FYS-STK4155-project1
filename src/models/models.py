5064d62248037e66039fd4f9703f997ad2ae6f17
Bjorn Gronntun <bjorngronntun@gmail.com>
2019-09-17 23:38:07 +0200

    Added ridge regression

--- src/models/models.py
+++ src/models/models.py
@@ -1,9 +1,18 @@
 import numpy as np
 
-class OLS():
+class OLS:
     def __init__(self):
         pass
     def fit(self, X, y):
         self.beta = np.dot(np.dot(np.linalg.inv(np.dot(X.transpose(), X)), X.transpose()), y)
+    def predict(self, X):
+        return np.dot(X, self.beta)
+
+class Ridge:
+    def __init__(self, lam):
+        self.lam = lam
+    def fit(self, X, y):
+        n = X.shape[1]
+        self.beta = np.dot(np.dot(np.linalg.inv(np.dot(X.transpose(), X) + self.lam*np.eye(n)), X.transpose()), y)
     def predict(self, X):
         return np.dot(X, self.beta)
